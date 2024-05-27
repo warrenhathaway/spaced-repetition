5b55f7c409c613fcb100ca2b2f369df9a7c0b50b
guohui <2806815594@qq.com>
2024-05-27 21:40:57 +0800

    promise实例和静态方法

--- js-code/promise/5.27.js
+++ js-code/promise/5.27.js
@@ -72,4 +72,135 @@ class MyPromise {
     })
   }
 
+  catch(onRejected) {
+    return this.then(null, onRejected)
+  }
+
+  finally(onSettled) {
+    return this.then((data) => {
+      onSettled()
+      return data
+    }, (reason) => {
+      onSettled()
+      throw reason
+    })
+  }
+ 
+  static resolve(data) {
+    if(data instanceof MyPromise) {
+      return data
+    }
+    return new myPromise((resolve, reject) => {
+      if(isPromise(data)) {
+        data.then(resolve, reject)
+      } else {
+        resolve(data)
+      }
+    })
+  }
+
+  static reject(reason) {
+    return new Promise((resolve, reject) => {
+      reject(reason)
+    })
+  }
+
+  static all(pros) {
+    return new MyPromise((resolve, reject) => {
+      try {
+        const result = []
+        let fulfilledCount = 0
+        let count = 0
+        for (const p of pros) {
+          let i = count
+          MyPromise.resolve(p).then((data) => {
+            result[i] = data
+            fulfilledCount++
+            if(fulfilledCount === count) resolve(result)
+          }, (reason) => {
+            reject(reason)
+          })
+          count++
+        }
+        if(count === 0) resolve(result)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  static allSettled(pros) {
+    const ps = []
+    for (const p of pros) {
+      ps.push(p.then((data) => {
+        return {
+          status: FULFILLED,
+          value: data
+        }
+      }, (reason) => {
+        return {
+          status: REJECTED,
+          reason: reason
+        }
+      }))
+    }
+    return this.all(ps)
+  }
+
+  static race(pros) {
+    return new MyPromise((resolve, reject) => {
+      for (const p of pros) {
+        MyPromise.resolve(p).then(resolve, reject)
+      }
+    })
+  }
+
+  static any(pros) {
+    return new MyPromise((resolve, reject) => {
+      try {
+        const result = []
+        let count = 0
+        let onRejectedCount = 0
+        for (const p of pros) {
+          let i = count
+          count++
+          MyPromise.resolve(p).then((res) => {
+            resolve(res)
+          }, (reason) => {
+            result[i] = reason
+            onRejectedCount++
+            if(onRejectedCount === count) reject(result)
+          })
+        }
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
 }
+
+let p = new MyPromise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve('aaa')
+    reject('aaa err')
+  }, 2000);
+});
+ 
+let p2 = new MyPromise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve('bbb')
+    reject('bbb err')
+  }, 1000);
+});
+
+const p3 = MyPromise.any([p, p2]);
+p3.then((res) => {
+  console.log('cg', res);
+}, err => {
+  console.log('sb', err);
+})
+setTimeout(() => {
+  // console.log(p);
+  console.log(p3);
+}, 3000);
